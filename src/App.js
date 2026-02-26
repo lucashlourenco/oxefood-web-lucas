77db87b32ff99c3182ddfd86e6b8224724b3029e
lucashlourenco <lucashlourencos@hotmail.com>
2026-02-25 21:14:53 -0300

    conteudo c10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Segment } from 'semantic-ui-react';
-import logo from './logo.svg';
 import FormCliente from './views/cliente/FormCliente';
 import './App.css';
-import FormProduto from './views/produto/FormProduto';
-import FormEntregador from './views/entregador/FormEntregador';
+//import FormProduto from './views/produto/FormProduto';
+//import FormEntregador from './views/entregador/FormEntregador';
 
 function App() {
   return (
     <div className="App">
-      <FormEntregador />
+      <FormCliente />
 
       <div style={{marginTop: '6%'}}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
